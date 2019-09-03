a3ca288a87fb8dd42cce7c56180d58a87eb0acf9
SantiagoOrtiz0630 <san.or.gue@gmail.com>
2019-09-03 01:35:08 -0500

    help -will change- have been added

--- Assets/Scripts/Utils/Help.cs
+++ Assets/Scripts/Utils/Help.cs
@@ -1,7 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Help : MonoBehaviour
 {
+
+    public GameObject m_HelpObject;
+    private Button m_Button;
+
+    private void Awake()
+    {
+        m_Button = GetComponentInChildren<Button>();
+
+        m_Button.onClick.AddListener(taskOnClick);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -13,5 +24,10 @@ public class Help : MonoBehaviour
     void Update()
     {
        
+    }
+
+    private void taskOnClick()
+    {
+        m_HelpObject.SetActive(!m_HelpObject.activeSelf);
     }
 }
